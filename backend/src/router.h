7bfdfb755d9bb8f4630965aeeaa9044975a69f15
carry <fcarriquiry@gmail.com>
2026-05-16 11:55:23 -0300

    lipio rama

--- backend/src/router.h
+++
@@ -1,9 +0,0 @@
-#ifndef ROUTER_H
-#define ROUTER_H
-
-#include "http_util.h"
-
-MhdResult route(struct MHD_Connection *conn, const char *method,
-                const char *url, const char *body);
-
-#endif
